52d5cf9514f2b9f578b1eb99983acd49a78e394b
Justin Warren <justinwarren@Justins-MacBook-Pro.local>
2019-10-18 20:17:33 -0400

    Created AJAX call and form submit button working

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 console.log($);
+
+//Window onload
+$(() => {
+
+      //This selects the form element and sets an event listener to run on submit.
+    $('form').on('submit', (event)=> {
+      //This prevents the page from reloading
+      event.preventDefault();
+      console.log("hi");
+      // This is the ajax call to get information from the weatherstack API
+        $.ajax({
+          url: "http://api.weatherstack.com/current?access_key=5dd8a14370cd45b1946ba62524b4683f&query=Chicago"
+        }).then(
+          (data)=>{  //If successful
+            console.log(data);  //Success callback will show if it works!
+          },
+          ()=>{  //If error and unsucceful in data request.
+            console.log('bad request');  //Error callback will trigger if doesn't work
+          }
+        );
+
+    });
+
+
+});
