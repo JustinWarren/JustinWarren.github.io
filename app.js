5a0178d436053ae78c3d2e871166f9ca5aff8771
Justin Warren <justinwarren@Justins-MacBook-Pro.local>
2019-10-18 20:30:47 -0400

    Created user input button and stored in variable logging ok

--- app.js
+++ app.js
@@ -8,12 +8,15 @@ $(() => {
       //This prevents the page from reloading
       event.preventDefault();
       console.log("hi");
+      //This stores the user input in the variable userInput
+      const userInput = $('#userInput1').val();
+      console.log(userInput);
       // This is the ajax call to get information from the weatherstack API
         $.ajax({
           url: "http://api.weatherstack.com/current?access_key=5dd8a14370cd45b1946ba62524b4683f&query=Chicago"
         }).then(
           (data)=>{  //If successful
-            console.log(data);  //Success callback will show if it works!
+            console.log(data.location.name);  //Success callback will show if it works!
           },
           ()=>{  //If error and unsucceful in data request.
             console.log('bad request');  //Error callback will trigger if doesn't work
@@ -24,3 +27,6 @@ $(() => {
 
 
 });
+
+
+// "http://api.weatherstack.com/current?access_key=5dd8a14370cd45b1946ba62524b4683f&query=Chicago"
